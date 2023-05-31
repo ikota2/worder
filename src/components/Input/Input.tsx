7468a286f02de938ac7f464d4213ea1f5c111061
Ivan Kotov <iamivankotov@gmail.com>
2023-06-01 00:13:40 +0200

    input props & styles

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -7,23 +7,28 @@ interface Props {
   label: string;
   value: string;
   onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  required?: boolean;
+  type?: string;
+  minLength?: number;
 }
 
-const Input: FC<Props> = ({label, value, onChange, name}) => {
+const Input: FC<Props> = ({type = 'text', label, value, onChange, name, required = false, minLength = 0}) => {
   return (
-    <div className={classes.container}>
-      <input
-        type="text"
-        id={name}
-        name={name}
-        autoComplete="off"
-        placeholder=""
-        className={classes.input}
-        value={value}
-        onChange={e => onChange(e)}
-      />
-      <label htmlFor={name} className={classes.label}>{label}</label>
-    </div>
+      <div className={classes.container}>
+        <input
+          type={type}
+          id={name}
+          name={name}
+          minLength={minLength}
+          autoComplete="off"
+          placeholder=""
+          className={classes.input}
+          value={value}
+          onChange={e => onChange(e)}
+          required={required}
+        />
+        <label htmlFor={name} className={classes.label}>{label}</label>
+      </div>
   );
 };
 
