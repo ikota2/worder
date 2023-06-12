3f56decab17b7ea6b41b2223b47a1b1396d93aea
Ivan Kotov <iamivankotov@gmail.com>
2023-06-12 23:05:34 +0200

    react-router-dom v5

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,16 +1,28 @@
 import React, {createContext, FC, useContext, useState, useEffect, ReactElement} from 'react';
 import { auth } from '../firebase';
-import { createUserWithEmailAndPassword } from "firebase/auth";
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut,
+  sendPasswordResetEmail,
+  updateEmail,
+  updatePassword,
+  Auth,
+} from "firebase/auth";
 
 interface User {
   email: string;
+  uid?: string;
 }
-
-//type User = { currentUser: User | null | undefined; signup: (auth: any, email: string, password: string) => Promise<UserCredential>; }' is not assignable to type '{ currentUser: User; signup: (auth: any, email: string, password: string) => void; },
 
 const AuthContext = createContext({
   currentUser: {} as User,
-  signup: (auth: any, email: string, password: string) => {}
+  signup: (auth: Auth, email: string, password: string) => {},
+  login: (auth: Auth, email: string, password: string) => {},
+  logout: (auth: Auth) => {},
+  resetPassword: (auth: Auth, email: string) => {},
+  changeEmail: (auth: any, email: string) => {},
+  changePassword: (auth: any, password: string) => {}
 });
 
 export function useAuth() {
@@ -22,8 +34,27 @@ const AuthProvider: FC<{children: ReactElement}> = ({children}) => {
   const [currentUser, setCurrentUser] = useState<any>({});
   const [loading, setLoading] = useState(true);
 
-  function signup(auth: any, email: string, password: string) {
+  function signup(auth: Auth, email: string, password: string) {
     return createUserWithEmailAndPassword(auth, email, password);
+  }
+
+  function login(auth: Auth, email: string, password: string) {
+    return signInWithEmailAndPassword(auth, email, password);
+  }
+
+  function logout(auth: Auth) {
+    return signOut(auth);
+  }
+
+  function resetPassword(auth: Auth, email: string) {
+    return sendPasswordResetEmail(auth, email)
+  }
+
+  function changeEmail(user: any, email: string) {
+    return updateEmail(user, email);
+  }
+  function changePassword(user: any, password: string) {
+    return updatePassword(user, password);
   }
 
   useEffect(() => {
@@ -31,13 +62,19 @@ const AuthProvider: FC<{children: ReactElement}> = ({children}) => {
       setCurrentUser(user as unknown as User);
       setLoading(false);
     });
+
     return unsubscribe;
   }, [])
 
 
   const value = {
     currentUser,
-    signup
+    signup,
+    login,
+    logout,
+    resetPassword,
+    changeEmail,
+    changePassword
   }
 
 
