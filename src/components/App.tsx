5d55a32a902d86413c6bc313e2a349616118e7d3
Ivan Kotov <iamivankotov@gmail.com>
2023-06-12 23:08:53 +0200

    App>Strict mode

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,13 +1,30 @@
-import React from 'react';
+import React, { StrictMode } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+import PrivateRoute from './PrivateRoute/PrivateRoute';
 import SignUp from '../features/SignUp/SignUp';
+import Dashboard from '../features/Dashboard/Dashboard';
+import LogIn from '../features/LogIn/LogIn';
+import ForgotPassword from '../features/ForgotPassword/ForgotPassword';
+import UpdateProfile from '../features/UpdateProfile/UpdateProfile';
 import AuthProvider from '../contexts/AuthContext';
 
 function App() {
 
   return (
-    <AuthProvider>
-      <SignUp />
-    </AuthProvider>
+    <Router>
+      <StrictMode>
+        <AuthProvider>
+          <Switch>
+            <PrivateRoute exact path="/" component={Dashboard} />
+            <PrivateRoute path="/update-profile" component={UpdateProfile} />
+            <Route path="/signup" component={SignUp} />
+            <Route path="/login" component={LogIn} />
+            <Route path="/forgot-password" component={ForgotPassword} />
+          </Switch>
+        </AuthProvider>
+      </StrictMode>
+    </Router>
   );
 }
 
